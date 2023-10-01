7ef199cf4026248448f4dbfd4e59abdfd52a2520
Enzo Hideo <enzohideo@gmail.com>
2023-10-01 15:27:53 -0300

    Add search bar
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>
    Signed-off-by: Lume <luisa_menezes@usp.br>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <v-app>
     <div>
+      <v-toolbar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Transites</v-toolbar-title>
+        <v-text-field
+          hide-details
+          prepend-inner-icon="mdi-magnify"
+          single-line
+        ></v-text-field>
+      </v-toolbar>
+    </div>
+    <div>
       <h1>Welcome to My Vue Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio vitae est
