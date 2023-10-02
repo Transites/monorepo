588e42617009dfab35913c95e519f3657060dba5
Luísa Menezes | Lume <luisa_menezes@usp.br>
2023-10-02 08:49:42 -0300

    Add about component
    Signed-off-by: Lume <luisa_menezes@usp.br>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,55 +1,14 @@
 <template>
   <v-app>
-    <v-container>
-      <v-row>
-        <v-col>
-          <div class="about">
-            <img src = "https://cdn.iconscout.com/icon/free/png-256/free-people-1767899-1502152.png">
-            <h1>
-            Sobre o Transites
-            </h1>
-          </div>
-          <p>
-          Saiba mais sobre nossa história e colaboradores.
-          </p>
-        </v-col>
 
-        <v-col>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio vitae est
-            gravida ultrices. Sed mattis, dolor eget luctus ultricies, arcu lectus cursus tortor, vel
-            ultricies tortor augue eget ipsum. Fusce tempus dolor libero, vel pharetra odio finibus
-            vel..
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio vitae est
-            gravida ultrices. Sed mattis, dolor eget luctus ultricies, arcu lectus cursus tortor, vel
-            ultricies tortor augue eget ipsum. Fusce tempus dolor libero, vel pharetra odio finibus
-            vel..
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio vitae est
-            gravida ultrices. Sed mattis, dolor eget luctus ultricies, arcu lectus cursus tortor, vel
-            ultricies tortor augue eget ipsum. Fusce tempus dolor libero, vel pharetra odio finibus
-            vel..
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et odio vitae est
-            gravida ultrices. Sed mattis, dolor eget luctus ultricies, arcu lectus cursus tortor, vel
-            ultricies tortor augue eget ipsum. Fusce tempus dolor libero, vel pharetra odio finibus
-            vel..
-          </p>
-
-        </v-col>
-      </v-row>
-    </v-container>
-
+    <About/>
     <Footer/>
 
   </v-app>
 </template>
 
 <script setup>
+import About from './components/About.vue'
 import Footer from './components/Footer.vue'
 </script>
 
