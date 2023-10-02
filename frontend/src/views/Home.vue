b16f121170ab286cdf5a18d81789409abd59de36
Luiz <luizcarlosdk@hotmail.com>
2023-10-02 18:54:10 -0300

    Add Banner Carousel on Home page.
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>
    Signed-off-by: Lume <luisa_menezes@usp.br>

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,22 @@
 <script setup>
 import About from '../components/About.vue'
 import News from '../components/News.vue'
+import Banner from '../components/Banner.vue'
+
 </script>
 
 <template>
   <main>
-    <About/>
+    <Banner/>
+    <div class="cor">
+      <About/>
+    </div>
     <News/>
   </main>
 </template>
+
+<style>
+.cor{
+  background-color: violet;
+}
+</style>
