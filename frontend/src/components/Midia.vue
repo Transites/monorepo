7b9d6a13fab89190f9603474654f6fedb4e7534f
Luiz <luizcarlosdk@hotmail.com>
2023-10-05 18:10:33 -0300

    WIP: Create an new session on homepage about midia
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

---
+++ frontend/src/components/Midia.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-container>
+      <iframe
+        v-for="(link, i) in links"
+        :key="i"
+        width="500"
+        height="315"
+        :src="link.url"
+        allowfullscreen
+      ></iframe>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      {
+        url: 'https://www.youtube.com/embed/'
+      },
+      { url: 'https://www.youtube.com/embed/' },
+      { url: 'https://www.youtube.com/embed/' }
+    ]
+  })
+}
+</script>
+
+<style>
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,18 @@
-<script setup>
-import About from '../components/About.vue'
-import News from '../components/News.vue'
-import Banner from '../components/Banner.vue'
-</script>
-
 <template>
   <main>
     <Banner />
     <About />
+    <Midia />
     <News />
   </main>
 </template>
+
+<script setup>
+import About from '../components/About.vue'
+import News from '../components/News.vue'
+import Banner from '../components/Banner.vue'
+import Midia from '../components/Midia.vue'
+</script>
 
 <style>
 </style>
