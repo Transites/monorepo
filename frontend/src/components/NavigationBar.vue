2a939be64019defdd278fe02da3075808b87ae7c
lume <luisa_menezes@usp.br>
2023-10-09 17:53:48 -0300

    Add advanced search button

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -2,9 +2,20 @@
   <v-toolbar>
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-toolbar-title>Transites</v-toolbar-title>
+    
+    <v-spacer></v-spacer>
+
     <v-text-field
       hide-details
       prepend-inner-icon="mdi-magnify"
     ></v-text-field>
+
+    <v-btn
+    rounded="lg"
+    prepend-icon="mdi-tune"
+    variant="flat">
+    Busca avançada
+    </v-btn>
+
   </v-toolbar>
 </template>
