50577974aa99c579a194a07eccc14266481a8358
Enzo Hideo <enzohideo@gmail.com>
2023-10-23 18:16:54 -0300

    Add responsivity to the 'News' component
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>

--- frontend/src/components/News.vue
+++ frontend/src/components/News.vue
@@ -1,28 +1,41 @@
 <template>
   <div>
     <h1>Novidades</h1>
-    <v-conteiner class="news-content">
-      <div class="d-flex">
-        <div v-for="column in entries" class="column" :key="column">
+    <v-conteiner>
+      <v-row align="center" justify="center">
+        <v-col cols="12" lg="9">
+          <v-row justify="space-between">
+            <v-col
+              cols="12"
+              sm="6"
+              v-for="entry in entries"
+              :key="entry"
+            >
+              <v-card
+                :title="entry.title"
+                :subtitle="entry.category"
+              >
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" lg="3" align="center">
           <v-card
-            class="news-cards"
-            v-for="entry in column"
-            :key="entry"
-            :title="entry.title"
-            :subtitle="entry.category"
+            width="100%"
+            max-height="450px"
+            style="aspect-ratio: auto 3/4"
           >
+            <v-img
+              class="align-end text-white"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
+              cover
+              align="start"
+            >
+              <v-card-title>Verbete</v-card-title>
+            </v-img>
           </v-card>
-        </div>
-      </div>
-      <v-card width="200" height="300">
-        <v-img
-          class="align-end text-white"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg"
-          cover
-        >
-          <v-card-title>Verbete</v-card-title>
-        </v-img>
-      </v-card>
+        </v-col>
+      </v-row>
     </v-conteiner>
   </div>
 </template>
@@ -31,63 +44,43 @@
 <script>
 export default {
   data: () => ({
-    entries: {
-      left: [
-        {
-          title: 'Título do verbete 1',
-          category: 'categoria 1'
-        },
-        {
-          title: 'Título do verbete 2',
-          category: 'categoria 2'
-        },
-        {
-          title: 'Título do verbete 3',
-          category: 'categoria 3'
-        },
-        {
-          title: 'Título do verbete 4',
-          category: 'categoria 4'
-        }
-      ],
-      right: [
-        {
-          title: 'Título do verbete 1',
-          category: 'categoria 1'
-        },
-        {
-          title: 'Título do verbete 2',
-          category: 'categoria 2'
-        },
-        {
-          title: 'Título do verbete 3',
-          category: 'categoria 3'
-        },
-        {
-          title: 'Título do verbete 4',
-          category: 'categoria 4'
-        }
-      ]
-    }
+    entries: [
+      {
+        title: 'Título do verbete 1',
+        category: 'categoria 1'
+      },
+      {
+        title: 'Título do verbete 2',
+        category: 'categoria 2'
+      },
+      {
+        title: 'Título do verbete 3',
+        category: 'categoria 3'
+      },
+      {
+        title: 'Título do verbete 4',
+        category: 'categoria 4'
+      },
+      {
+        title: 'Título do verbete 1',
+        category: 'categoria 1'
+      },
+      {
+        title: 'Título do verbete 2',
+        category: 'categoria 2'
+      },
+      {
+        title: 'Título do verbete 3',
+        category: 'categoria 3'
+      },
+      {
+        title: 'Título do verbete 4',
+        category: 'categoria 4'
+      }
+    ]
   })
 }
 </script>
+
 <style>
-.column {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-}
-
-.news-content {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  flex-wrap: wrap;
-}
-
-.news-cards {
-  margin: 0.5rem;
-  align-items: flex-start;
-}
 </style>
