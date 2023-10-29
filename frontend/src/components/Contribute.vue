75335ac98add1f43b20ede6c4ac8ae58d968f655
Enzo Hideo <enzohideo@gmail.com>
2023-10-29 18:00:59 -0300

    Add functionality to the Contribute form
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

--- frontend/src/components/Contribute.vue
+++ frontend/src/components/Contribute.vue
@@ -4,38 +4,25 @@
       <v-col cols="4">
         <div class="contribute-container">
           <div class="d-flex flex-row">
-            <img class="contribute-icon" src="https://cdn-icons-png.flaticon.com/512/1105/1105791.png">
-            <h1 class="contribute-title">Contribua com <br>o Transites</h1>
+            <img
+              class="contribute-icon"
+              src="https://cdn-icons-png.flaticon.com/512/1105/1105791.png"
+            />
+            <h1 class="contribute-title">Contribua com <br />o Transites</h1>
           </div>
-          <p>Envie sua sugestão, elogio ou crítica...
-          <br><br>
-          ...ou contribua com um verbete! (Clique <u>aqui</u> para conferir nossas normas de publicação)
+          <p>
+            Envie sua sugestão, elogio ou crítica... <br /><br />
+            ...ou contribua com um verbete! (Clique <u>aqui</u> para conferir nossas normas de
+            publicação)
           </p>
         </div>
       </v-col>
       <v-col cols="8">
-        <v-form>
-          <v-text-field
-            v-model="firstName"
-            :rules="rules"
-            label="Nome completo"
-          ></v-text-field>
-          <v-text-field
-            v-model="email"
-            :rules="rules"
-            label="E-mail"
-          ></v-text-field>
-          <v-text-field
-            v-model="subject"
-            :rules="rules"
-            label="Assunto"
-          ></v-text-field>
-          <v-textarea
-            v-model="message"
-            :rules="rules"
-            label="Mensagem"
-          ></v-textarea>
-          <v-btn type="submit" block class="mt-2">Enviar</v-btn>
+        <v-form ref="emailForm">
+          <v-text-field v-model="firstName" :rules="rules" label="Nome completo"></v-text-field>
+          <v-text-field v-model="subject" :rules="rules" label="Assunto"></v-text-field>
+          <v-textarea v-model="message" :rules="rules" label="Mensagem"></v-textarea>
+          <v-btn type="submit" block class="mt-2" @click.prevent="sendEmail">Enviar</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -46,19 +33,30 @@
 export default {
   data: () => ({
     firstName: '',
-    email: '', 
-    subject: '', 
-    message: '', 
+    subject: '',
+    message: '',
     rules: [
-      value => {
+      (value) => {
         if (value) return true
         return 'Campo obrigatório.'
-      },
-    ],
+      }
+    ]
   }),
+  methods: {
+    sendEmail() {
+      const email = import.meta.env.VITE_CONTRIBUTE_EMAIL
+      const body = this.message.replace(/\n/g, '%0D')
+      this.$refs.emailForm.validate().then((result) => {
+        if (result.valid) {
+          window.open(
+            `mailto:${email}?subject=${this.subject}&body=${body}%0D%0DDe: ${this.firstName}`
+          )
+        }
+      })
+    }
+  }
 }
 </script>
-
 
 <style scoped>
 .contribute-container {
