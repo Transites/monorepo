d1c9f51bcf0ed2823ef4937f6403028c518641e5
Enzo Hideo <enzohideo@gmail.com>
2023-11-13 17:57:50 -0300

    Update SearchView to use v-form
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -1,44 +1,66 @@
 <template>
   <div style="padding-top: 80px">
-    <v-row>
-      <v-col cols="12">
-        <v-select
-          v-model="Search.PrimaryValue"
-          :items="PrimaryTags"
-          chips
-          label="Categoria"
-          multiple
-        ></v-select>
-      </v-col>
-      <v-col>
-        <v-select
-          v-model="Search.SecondaryValue"
-          :items="SecondaryTags"
-          chips
-          label="Tipo de Verbete"
-          multiple
-        ></v-select>
-      </v-col>
-      <v-col>
-        <v-btn>Buscar</v-btn>
-      </v-col>
-    </v-row>
+    <v-form ref="emailForm">
+      <v-container>
+        <v-row>
+          <v-text-field label="Pesquisa Textual"></v-text-field>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-select
+              v-model="Search.PrimaryValue"
+              :items="PrimaryTags"
+              chips
+              label="Categoria"
+              multiple
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-select
+              v-model="Search.SecondaryValue"
+              :items="SecondaryTags"
+              chips
+              label="Tipo de Verbete"
+              multiple
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field type="date" label="De"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field type="date" label="Até"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn
+            class="text-white"
+            type="submit"
+            color="var(--transites-red)"
+            @click.prevent="applySearch"
+            block
+            >Buscar</v-btn
+          >
+        </v-row>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-
     Search: {
       PrimaryValue: [],
-      SecondaryValue: [],
+      SecondaryValue: []
     },
-    PrimaryTags: ['Artes','Ciência'],
-    SecondaryTags: ['Pessoa', 'Obra', 'Instituição', 'Logradouro'],
-
-
-  })
+    PrimaryTags: ['Artes', 'Ciências'],
+    SecondaryTags: ['Pessoa', 'Obra', 'Instituição', 'Logradouro']
+  }),
+  methods: {
+    applySearch() {}
+  }
 }
 </script>
 
