c0ae41e459a4f9a59ab1215adef3598b3a657823
Enzo Hideo <enzohideo@gmail.com>
2023-12-04 03:41:26 -0300

    Add SearchBar component and remove button text
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,47 +1,41 @@
 <template>
   <div>
     <v-app-bar flat fixed style="background-color: white">
-      <v-app-bar-nav-icon
-        class="hidden-md-and-up"
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
-
       <div style="cursor: pointer" @click="$router.push('/')" class="titleIcon">
         <img src="../assets/transites-icon.svg" alt="Icon" height="50" width="50" style="margin: 0 10px 0 10px">
         <v-toolbar-title><b class="title">Transites</b></v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
-      <v-text-field
-        class="hidden-sm-and-down"
-        clearable
-        rounded
-        variant="solo"
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-      ></v-text-field>
-      <v-btn
-        class="hidden-sm-and-down text-white"
-        rounded="lg"
-        prepend-icon="mdi-tune"
-        variant="flat"
-        color="var(--transites-red)"
-      >
-        Busca avançada
-      </v-btn>
+      <v-spacer></v-spacer>
+
+      <SearchBar class="hidden-sm-and-down mr-3"/>
+
+      <template #append>
+        <v-app-bar-nav-icon
+          class="hidden-md-and-up"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+      </template>
+
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" temporary>
-      <v-text-field label="Pesquisa"></v-text-field>
-      <v-btn>Busca Avançada</v-btn>
+
+    <v-navigation-drawer class="hidden-md-and-up" location="right" v-model="drawer" temporary>
+      <SearchBar class="mx-4" />
     </v-navigation-drawer>
   </div>
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar.vue'
+
 export default {
   data: () => ({
     drawer: false
-  })
+  }),
+  components: {
+    SearchBar
+  }
 }
 </script>
 
---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-text-field
+    color="var(--transites-red)"
+    clearable
+    rounded="xl"
+    variant="outlined"
+    hide-details
+    prepend-inner-icon="mdi-magnify"
+    density="compact"
+    append-inner-icon="mdi-tune"
+    @click:append-inner="openAdvancedSearch"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  methods: {
+    openAdvancedSearch() {
+      // TODO route to advanced-search page
+      this.$router.push('/');
+    }
+  }
+}
+</script>
