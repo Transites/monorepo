e32f3a9c43a48cf409065a86222897a65d769764
Enzo Hideo <enzohideo@gmail.com>
2023-12-04 19:29:32 -0300

    Avoid redirect loop if Article is not found
    Signed-off-by: Enzo Hideo <enzohideo@gmail.com>

--- frontend/src/views/Article.vue
+++ frontend/src/views/Article.vue
@@ -1,105 +1,111 @@
 <template>
-  <v-progress-linear indeterminate v-if="!article" color="var(--transites-red)"></v-progress-linear>
-  <div style="padding: 0 30px 0 30px" v-if="!!article">
-    <div style="color: var(--transites-red)">
-      <div>
-        <h1>{{ article.attributes.title }}</h1>
-        <h2>{{ article.attributes.alternativeTitles }}</h2>
-        <h3 v-if="article.attributes.authors.data.length > 0">
-          Author:
-          <span v-for="(author, index) in article.attributes.authors.data" :key="author">
-            <span>{{ author.attributes.name }}</span>
-            <span v-if="index + 1 < article.attributes.authors.data.length">, </span>
-          </span>
-        </h3>
-        <p>
-          Publicado em: {{ formatDateToLocale(article.attributes.publishedAt) }} | Atualizado em:
-          {{ formatDateToLocale(article.attributes.updatedAt) }}
-        </p>
-      </div>
-
-      <v-divider thickness="5" class="border-opacity-100" style="margin: 10px 0 5px"></v-divider>
-
-      <div>
-        <v-btn
-          class="tags text-white"
-          v-for="category in article.attributes.categories.data"
-          :key="category"
-          color="var(--transites-red)"
-          rounded
-          style="margin: 5px 10px 5px 0px"
-        >
-          {{ category.attributes.name }}
-        </v-btn>
-        <v-btn
-          class="tags text-white"
-          v-for="tag in article.attributes.tags.data"
-          :key="tag"
-          color="var(--transites-red)"
-          rounded
-          style="margin: 5px 10px 5px 0px"
-        >
-          {{ tag.attributes.name }}
-        </v-btn>
-      </div>
-
-      <v-divider thickness="5" class="border-opacity-100" style="margin: 5px 0 10px"></v-divider>
-
-      <div>
-        <h3 v-if="!!article.attributes.birth">
-          Nascimento: {{ article.attributes.birth.place }}, {{ article.attributes.birth.date }}
-        </h3>
-        <h3 v-if="!!article.attributes.death">
-          Falecimento: {{ article.attributes.death.place }}, {{ article.attributes.death.date }}
-        </h3>
-      </div>
+  <div>
+    <v-progress-linear indeterminate v-if="!article && !error" color="var(--transites-red)"></v-progress-linear>
+    <div class="d-flex align-center justify-center" style="margin: 12vh 20px 20vh 20px" v-if="error">
+      <NotFound />
     </div>
+    <div style="padding: 0 30px 0 30px" v-if="!!article">
+      <div style="color: var(--transites-red)">
+        <div>
+          <h1>{{ article.attributes.title }}</h1>
+          <h2>{{ article.attributes.alternativeTitles }}</h2>
+          <h3 v-if="article.attributes.authors.data.length > 0">
+            Author:
+            <span v-for="(author, index) in article.attributes.authors.data" :key="author">
+              <span>{{ author.attributes.name }}</span>
+              <span v-if="index + 1 < article.attributes.authors.data.length">, </span>
+            </span>
+          </h3>
+          <p>
+            Publicado em: {{ formatDateToLocale(article.attributes.publishedAt) }} | Atualizado em:
+            {{ formatDateToLocale(article.attributes.updatedAt) }}
+          </p>
+        </div>
 
-    <v-container style="color: var(--transites-red)" fluid>
-      <v-row>
-        <v-card
-          style="border-width: 4px; margin: 20px"
-          variant="outlined"
-          width="min(400px, 100%)"
-          class="rounded-lg"
-          v-if="!!article.attributes.image.data"
-        >
-          <v-img :src="article.attributes.image.data.attributes.formats.small.url" cover></v-img>
-          <v-card-title>
-            {{ article.attributes.image.data.attributes.caption }}
-          </v-card-title>
-        </v-card>
-        <v-col cols="12" md="6" style="padding: 20px" v-if="!!article.attributes.summary">
-          <div v-html="markdown.render(article.attributes.summary)"></div>
-        </v-col>
-      </v-row>
-    </v-container>
+        <v-divider thickness="5" class="border-opacity-100" style="margin: 10px 0 5px"></v-divider>
 
-    <div>
-      <v-expansion-panels multiple>
-        <v-expansion-panel
-          class="section"
-          v-for="(section, index) in article.attributes.sections"
-          :key="section"
-          :style="{ color: getPanelColor(index, article.attributes.sections.length) }"
-        >
-          <v-divider thickness="5" class="border-opacity-100"></v-divider>
-          <v-expansion-panel-title style="font-size: x-large">
-            {{ section.title }}
-          </v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <div
-              :class="{ markdown: section.title === 'Artigo' }"
-              v-html="markdown.render(section.content)"
-            ></div>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
+        <div>
+          <v-btn
+            class="tags text-white"
+            v-for="category in article.attributes.categories.data"
+            :key="category"
+            color="var(--transites-red)"
+            rounded
+            style="margin: 5px 10px 5px 0px"
+          >
+            {{ category.attributes.name }}
+          </v-btn>
+          <v-btn
+            class="tags text-white"
+            v-for="tag in article.attributes.tags.data"
+            :key="tag"
+            color="var(--transites-red)"
+            rounded
+            style="margin: 5px 10px 5px 0px"
+          >
+            {{ tag.attributes.name }}
+          </v-btn>
+        </div>
+
+        <v-divider thickness="5" class="border-opacity-100" style="margin: 5px 0 10px"></v-divider>
+
+        <div>
+          <h3 v-if="!!article.attributes.birth">
+            Nascimento: {{ article.attributes.birth.place }}, {{ article.attributes.birth.date }}
+          </h3>
+          <h3 v-if="!!article.attributes.death">
+            Falecimento: {{ article.attributes.death.place }}, {{ article.attributes.death.date }}
+          </h3>
+        </div>
+      </div>
+
+      <v-container style="color: var(--transites-red)" fluid>
+        <v-row>
+          <v-card
+            style="border-width: 4px; margin: 20px"
+            variant="outlined"
+            width="min(400px, 100%)"
+            class="rounded-lg"
+            v-if="!!article.attributes.image.data"
+          >
+            <v-img :src="article.attributes.image.data.attributes.formats.small.url" cover></v-img>
+            <v-card-title>
+              {{ article.attributes.image.data.attributes.caption }}
+            </v-card-title>
+          </v-card>
+          <v-col cols="12" md="6" style="padding: 20px" v-if="!!article.attributes.summary">
+            <div v-html="markdown.render(article.attributes.summary)"></div>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <div>
+        <v-expansion-panels multiple>
+          <v-expansion-panel
+            class="section"
+            v-for="(section, index) in article.attributes.sections"
+            :key="section"
+            :style="{ color: getPanelColor(index, article.attributes.sections.length) }"
+          >
+            <v-divider thickness="5" class="border-opacity-100"></v-divider>
+            <v-expansion-panel-title style="font-size: x-large">
+              {{ section.title }}
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <div
+                :class="{ markdown: section.title === 'Artigo' }"
+                v-html="markdown.render(section.content)"
+              ></div>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import NotFound from '../components/NotFound.vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
 import markdownIt from 'markdown-it'
@@ -122,7 +128,8 @@ export default {
         '--transites-yellow',
         '--transites-blue',
         '--transites-gray-purple'
-      ]
+      ],
+      error: false
     }
   },
   methods: {
@@ -146,9 +153,12 @@ export default {
       axios.get(`${base_url}/api/${type}-articles/${id}?populate=*`).then((response) => {
         this.article = response.data.data
       }).catch(error => {
-        this.$router.push('/404')
+        this.error = true;
       });
     }
+  },
+  components: {
+    NotFound: NotFound
   }
 }
 </script>
