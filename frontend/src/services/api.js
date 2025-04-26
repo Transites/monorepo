0abb54070302b7bb021d3310793cf13006f01f8e
Gustavo Araujo 🐋 <guaraujo@microsoft.com>
2025-04-25 21:12:34 -0300

    update to use env variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:1337/api', // URL do seu backend Strapi
+  baseURL: 'http://enciclopedia.iea.usp.br:1337/api', // URL do seu backend Strapi
 });
 
 export default api;
