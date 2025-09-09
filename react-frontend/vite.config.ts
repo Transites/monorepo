619d2edaf519f596e6c0a26c87083adbcc770652
Gustavo Araujo 🐋 <gus.araujo@outlook.com>
2025-09-09 20:57:08 -0300

    Update Vite config: specify allowed host and enhance server settings

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import removeConsole from "vite-plugin-remove-console";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "production" && removeConsole(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => ({
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+        allowedHosts: ['enciclopedia.iea.usp.br']
     },
-  },
+    plugins: [
+        react(),
+        mode === "production" && removeConsole(),
+    ].filter(Boolean),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
