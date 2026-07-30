57b30b077bc26c5241636dbf22718749cf7e47f2
ZaWarudo <vinicius.paz@zrp.com.br>
2026-07-30 13:31:40 -0300

    fix: placeholder in register and login

--- react-frontend/src/pages/Register.tsx
+++ react-frontend/src/pages/Register.tsx
@@ -82,7 +82,7 @@ const Register = () => {
                       {...register("email")}
                       id="email"
                       type="email"
-                      placeholder="exemplo@usp.br"
+                      placeholder="exemplo@gmail.com"
                       className={cn(
                         "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
                         errors.email && "border-destructive focus-visible:ring-destructive"
